b13e67a8ccd8da56742c611cae27e7df206c6204
Ben Galley <bengalley21@gmail.com>
2020-04-14 10:50:04 +0100

    added user name column to users

--- db/migrate/20170526114520_create_posts.rb
+++ db/migrate/20170526114520_create_posts.rb
@@ -4,6 +4,7 @@ class CreatePosts < ActiveRecord::Migration[5.1]
   def change
     create_table :posts do |t|
       t.string :message
+      t.references :users, index: true, foreign_key: true
 
       t.timestamps
     end
