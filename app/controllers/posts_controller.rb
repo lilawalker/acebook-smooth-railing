d370f21cee988d8163be6be0b8e5f2f825eb7185
Ben Galley <bengalley21@gmail.com>
2020-04-15 11:43:30 +0100

    added destroy method and messages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,11 +13,32 @@ class PostsController < ApplicationController
   def index
     @posts = Post.order(:created_at).reverse_order
   end
+  
+  def destroy
+    find_post_by_id
+    p @post
+    if @post.user_id == current_user.id
+      @post.destroy
+      # redirect_to(:posts)
+      respond_to do |format|
+        format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
+      end
+    else
+      respond_to do |format|
+        format.html { redirect_to posts_url, alert: 'Error. You can only delete your own posts!' }
+      end
+    end  
+      
+  end
 
   private
 
   def post_params
     params.require(:post).permit(:message, :user_id, :name)
+  end
+
+  def find_post_by_id
+    @post = Post.find(params[:id])
   end
 
 end
