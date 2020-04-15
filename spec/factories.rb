45681e8be3c53dad8d4c338e6fe2258d5b4b2500
naeglinghaff <kate.o.haffenden@gmail.com>
2020-04-15 14:56:58 +0100

    Updated tests for delete and update posts, red

--- spec/factories.rb
+++ spec/factories.rb
@@ -5,4 +5,11 @@ FactoryBot.define do
     email { "fake@email.com" }
     password { "password" }
   end
+
+  factory :post do
+    message { "Fake news" }
+    id { 1 }
+    user_id { 40 }
+    created_at { "2020-04-15 12:18:33.798988" }
+  end
 end
